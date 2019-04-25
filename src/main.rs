893a8cdcbf4fe5bba7b5639938593f1f52cbd414
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-04-25 14:30:50 +0200

    Added a (so far) fake wasm loader and executor alongside the simulator

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,24 @@ extern crate serde_json;
 mod add;
 mod implementation;
 mod wrapper;
+mod wasm;
 
 use implementation::Implementation;
 use crate::wrapper::Wrapper;
 use add::Add;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let inputs = vec!(vec!(json!(1)), vec!(json!(2)));
     let implementation = &Wrapper{implementation: &Add{}} as &Implementation;
+    let (result, run_again) = implementation.run(inputs.clone());
+    println!("Result = {}, run_again = {}", result.unwrap(), run_again);
 
-    let (result, run_again) = implementation.run(inputs);
-
+    let mut buffer = Vec::new();
+    let mut file = File::open("add.wasm").unwrap();
+    file.read_to_end(&mut buffer).unwrap();
+    let wasm_executor = wasm::load("add", buffer);
+    let (result, run_again) = wasm_executor.run(inputs);
     println!("Result = {}, run_again = {}", result.unwrap(), run_again);
 }
---
+++ src/wasm.rs
@@ -0,0 +1,53 @@
+use std::sync::{Arc, Mutex};
+
+use crate::implementation::Implementation;
+use crate::implementation::RunAgain;
+use serde_json::Value;
+use wasmi::{Module, ModuleRef, ModuleInstance, ImportsBuilder};
+//use wasmi::{Module, ModuleRef, ModuleInstance, ImportsBuilder, RuntimeValue, NopExternals};
+
+use serde_json;
+
+pub struct WasmExecutor {
+    pub module: Arc<Mutex<ModuleRef>>,
+    function_name: String,
+}
+
+impl Implementation for WasmExecutor {
+    fn run(&self, _inputs: Vec<Vec<Value>>) -> (Option<Value>, RunAgain) {
+        println!("Wasm implementation wrapper for function '{}' called", self.function_name);
+
+        // TODO setup module memory
+        // TODO call the wasm implementation function (by name?) and get the result
+        // TODO read the module memory and reconstruct the return tuple
+
+        /*
+          A wasm module is invoked thus:
+            pub fn invoke_export<E: Externals>(&self, func_name: &str, args: &[RuntimeValue],
+                                    externals: &mut E) -> Result<Option<RuntimeValue>, Error>
+
+        let res = module.invoke_export(self.function_name, &[RuntimeValue::from(inputs)],
+                                  &mut NopExternals).unwrap().unwrap();
+        res
+        */
+        (Some(json!(42)), true)
+    }
+}
+
+/*
+    load a Wasm module from the specified Url.
+*/
+pub fn load(function_name: &str, content: Vec<u8>) -> Arc<WasmExecutor> {
+    let module = Module::from_buffer(content).unwrap();
+
+    let module_ref = ModuleInstance::new(&module, &ImportsBuilder::default())
+        .unwrap()
+        .assert_no_start();
+
+    let executor = WasmExecutor {
+        module: Arc::new(Mutex::new(module_ref.clone())),
+        function_name: function_name.to_string()
+    };
+
+    Arc::new(executor)
+}
