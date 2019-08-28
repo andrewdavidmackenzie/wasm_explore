54af2323efadbc24edc1b42119ea244812ab082a
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-08-28 17:17:54 +0100

    Added a derive macro that adds the code you need!!!

--- add_function/src/add_function.rs
+++ add_function/src/add_function.rs
@@ -2,44 +2,14 @@ extern crate core;
 extern crate implementation;
 #[macro_use]
 extern crate serde_json;
-
-use serde_json::Value;
+extern crate wasm_impl_derive;
 
 use implementation::Implementation;
+use serde_json::Value;
+use wasm_impl_derive::WasmImpl;
 
+#[derive(WasmImpl)]
 pub struct Add;
-use std::mem;
-use std::os::raw::c_void;
-use std::ptr::copy;
-
-/*
-    Allocate a chunk of memory of `size` bytes in wasm module
-*/
-#[no_mangle]
-pub extern "C" fn alloc(size: usize) -> *mut c_void {
-    let mut buf = Vec::with_capacity(size);
-    let ptr = buf.as_mut_ptr();
-    mem::forget(buf);
-    return ptr as *mut c_void;
-}
-
-#[no_mangle]
-pub extern "C" fn run_wasm(input_data_ptr: *mut c_void, input_data_length: i32) -> i32 {
-    let input_data: Vec<u8> = unsafe {
-        Vec::from_raw_parts(input_data_ptr as *mut u8,
-                              input_data_length as usize, input_data_length as usize)
-    };
-
-    let inputs = serde_json::from_slice(&input_data).unwrap();
-    let adder = Add {};
-    let result = adder.run(inputs);
-
-    let return_data = serde_json::to_vec(&result).unwrap();
-
-    unsafe { copy(return_data.as_ptr(), input_data_ptr as *mut u8, return_data.len()); }
-
-    return_data.len() as i32
-}
 
 impl Implementation for Add {
     fn run(&self, inputs: Vec<Vec<Value>>) -> (Option<Value>, bool) {
